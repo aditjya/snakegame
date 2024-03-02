68ecae414da66f19dfdf6dd6f2715fd51dc6f573
Koneti Adithya Prasad <150054066+aditjya@users.noreply.github.com>
2024-03-02 12:35:28 +0530

    Initial commit

---
+++ SnakeGame.java
@@ -0,0 +1,22 @@
+package snakegame;
+
+import javax.swing.*;
+
+ 
+
+public class SnakeGame extends JFrame{
+    
+    
+    SnakeGame(){
+        super("Snake Game");
+        add(new Board()) ;//snake environment
+        pack();//refreshes the frame
+        setLocationRelativeTo(null);
+        setResizable(false);
+    }
+   public static void main(String[] args) {
+       
+       new SnakeGame().setVisible( true);
+
+    } 
+}
